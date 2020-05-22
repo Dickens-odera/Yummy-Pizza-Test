986b08dd20a11a81a6cbf0f08d3fdd76e0d1ff24
Dickens-odera <dickensodera9@gmail.com>
2020-05-22 13:43:19 -0400

    added pizza api resource

---
+++ app/Http/Resources/PizzaApiResource.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PizzaApiResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        //return parent::toArray($request);
+        return array(
+            'id'=>$this->id,
+            'title'=>$this->title,
+            'description'=>$this->description,
+            'avartar'=>$this->avartar
+        );
+    }
+    public function with($request)
+    {
+        return array(
+            'version'=>'0.0.1',
+            'author'=>array(
+                'name'=>'Dickens Odera',
+                'email'=>'dickensodera9@gmail.com',
+                'github'=>'Dickens-odera',
+                'social'=>array(
+                    'twitter'=>'@dickensodera',
+                    'facebook'=>'dickens.odera',
+                    'linkedin'=>'Dickens Odera'
+                )
+            )
+        );
+    }
+}
--- tests/Feature/ShoppingCartTest.php
+++ tests/Feature/ShoppingCartTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Pizzas;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -26,6 +27,15 @@ class ShoppingCartTest extends TestCase
     public function test_that_a_pizza_item_can_be_added()
     {
         $this->withoutExceptionHandling();
-        
+        $items = factory(Pizzas::class)->make();
+        $response = $this->post(route('pizzas.api.add'),[
+            'title'=>$items->title,
+            'description'=>$items->description,
+            'price'=>$items->price,
+            'avartar'=>$items->avartar
+        ]);
+
+        $response->assertOk();
+        $this->assertCount(1, Pizzas::all());
     }
 }
