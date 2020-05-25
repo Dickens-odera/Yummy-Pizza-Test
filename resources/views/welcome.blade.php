713804909ddf0ae93ef2fd195bfa476eadaca032
Dickens-odera <dickensodera9@gmail.com>
2020-05-25 07:22:43 -0400

    modified js asset rendoring

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,8 +29,8 @@
     </head>
     <body>
        <div id="app"></div>
-       <script src="{{ asset('js/app.js') }}"></script>
-
+       <script src="js/app.js"></script>
+       {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
       <!--JavaScript at end of body for optimized loading-->
       {{-- <script type="text/javascript" src="{{ asset('js/materialize.min.js') }}"></script> --}}
 
